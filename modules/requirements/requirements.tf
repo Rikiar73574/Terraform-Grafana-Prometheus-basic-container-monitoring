739fbf1dcedbbbbd95cb7d401a1096887ab5e18c
loseryoda <31484379+loseryoda@users.noreply.github.com>
2023-08-16 22:02:15 +0200

    divided main into requirement and grafana module

---
+++ modules/requirements/requirements.tf
@@ -0,0 +1,61 @@
+variable "choco_installed" {
+  description = "Is Chocolatey installed?"
+  type        = bool
+  default     = false
+}
+
+variable "terraform_installed" {
+  description = "Is Terraform installed via Chocolatey?"
+  type        = bool
+  default     = false
+}
+
+variable "docker_desktop_installed" {
+  description = "Is Docker Desktop installed via Chocolatey?"
+  type        = bool
+  default     = false
+}
+
+resource "null_resource" "check_requirements" {
+  provisioner "local-exec" {
+    command = <<EOF
+      powershell -command "if (Get-Command choco -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_choco_installed -Value true -Scope Global }"
+      powershell -command "if (Get-Command terraform -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_terraform_installed -Value true -Scope Global }"
+      powershell -command "if (Get-Command docker -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_docker_desktop_installed -Value true -Scope Global }"
+    EOF
+  }
+}
+
+resource "null_resource" "install_choco" {
+  provisioner "local-exec" {
+    command = <<EOF
+      powershell -command "if (-not (Get-Variable -Name TF_VAR_choco_installed -ErrorAction SilentlyContinue)) { .\choco-install.ps1 }"
+    EOF
+  }
+}
+
+resource "null_resource" "install_terraform" {
+  provisioner "local-exec" {
+    command = <<EOF
+      powershell -command "if (-not (Get-Variable -Name TF_VAR_terraform_installed -ErrorAction SilentlyContinue)) { .\terraform-install.ps1 }"
+    EOF
+  }
+}
+
+resource "null_resource" "install_docker_desktop" {
+  provisioner "local-exec" {
+    command = <<EOF
+      powershell -command "if (-not (Get-Variable -Name TF_VAR_docker_desktop_installed -ErrorAction SilentlyContinue)) { .\docker_desktop-install.ps1 }"
+    EOF
+  }
+}
+
+resource "null_resource" "configure_docker_desktop" {
+  depends_on = [null_resource.install_docker_desktop]
+
+  provisioner "local-exec" {
+    command = <<EOF
+      powershell -command "if ((Get-Variable -Name TF_VAR_docker_desktop_installed -ErrorAction SilentlyContinue) -or (Get-Command docker -ErrorAction SilentlyContinue)) { .\docker_desktop-configure.ps1 }"
+    EOF
+  }
+}
