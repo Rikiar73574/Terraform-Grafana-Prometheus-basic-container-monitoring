2b5655261f5cbf4cd2e6081a3b1a6cf30e273168
loseryoda <31484379+loseryoda@users.noreply.github.com>
2023-08-19 23:25:26 +0200

    Script ExecutionPolicy Bypass

--- requirements.tf
+++ requirements.tf
@@ -16,22 +16,14 @@ variable "docker_desktop_installed" {
   default     = false
 }
 
-resource "null_resource" "check_requirements" {
-  provisioner "local-exec" {
-    command = <<EOF
-      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\choco-check.ps1' -Verb RunAs}"
-      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\terraform-check.ps1' -Verb RunAs}"
-      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\docker_desktop-check.ps1' -Verb RunAs}"
-    EOF
-  }
-}
+//to be done: add check for all three programs
 
 resource "null_resource" "install_choco" {
   count = var.choco_installed ? 0 : 1
 
   provisioner "local-exec" {
     command = <<EOF
-      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\choco-install.ps1' -Verb RunAs}"
+      powershell.exe -ExecutionPolicy Bypass -File ${abspath(path.module)}\Terraform_Helpers\choco-install.ps1 
     EOF
   }
 }
@@ -41,7 +33,7 @@ resource "null_resource" "install_terraform" {
 
   provisioner "local-exec" {
     command = <<EOF
-      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\terraform-install.ps1' -Verb RunAs}"
+      powershell.exe -ExecutionPolicy Bypass -File ${abspath(path.module)}\Terraform_Helpers\terraform-install.ps1 
     EOF
   }
 }
@@ -51,7 +43,7 @@ resource "null_resource" "install_docker_desktop" {
 
   provisioner "local-exec" {
     command = <<EOF
-      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\docker_desktop-install.ps1' -Verb RunAs}"
+      powershell.exe -ExecutionPolicy Bypass -File ${abspath(path.module)}\Terraform_Helpers\docker-desktop-install.ps1 
     EOF
   }
 }
@@ -63,7 +55,7 @@ resource "null_resource" "configure_docker_desktop" {
   count = var.docker_desktop_installed ? 0 : 1
  provisioner "local-exec" {
    command = <<EOF
-     powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\\Terraform_Helpers\\docker_desktop-configure.ps1' -Verb RunAs}"
+     powershell.exe -ExecutionPolicy Bypass -File ${abspath(path.module)}\Terraform_Helpers\docker-configure.ps1 
    EOF
  }
  
