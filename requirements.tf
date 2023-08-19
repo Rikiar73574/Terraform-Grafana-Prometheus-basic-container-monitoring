bfcd7adfa6674b4c34d47311e82f6f9b304dd2ee
loseryoda <31484379+loseryoda@users.noreply.github.com>
2023-08-19 21:54:08 +0200

    new attempt to run ps1 scripts

--- requirements.tf
+++ requirements.tf
@@ -19,9 +19,9 @@ variable "docker_desktop_installed" {
 resource "null_resource" "check_requirements" {
   provisioner "local-exec" {
     command = <<EOF
-      powershell -command "Start-Process -FilePath 'powershell' -ArgumentList '-Command if (Get-Command choco -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_choco_installed -Value true -Scope Global }' -Verb RunAs"
-      powershell -command "Start-Process -FilePath 'powershell' -ArgumentList '-Command if (Get-Command terraform -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_terraform_installed -Value true -Scope Global }' -Verb RunAs"
-      powershell -command "Start-Process -FilePath 'powershell' -ArgumentList '-Command if (Get-Command docker -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_docker_desktop_installed -Value true -Scope Global }' -Verb RunAs"
+      Start-Process -FilePath 'powershell' -ArgumentList '-Command if (Get-Command choco -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_choco_installed -Value true -Scope Global }' -Verb RunAs
+      Start-Process -FilePath 'powershell' -ArgumentList '-Command if (Get-Command terraform -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_terraform_installed -Value true -Scope Global }' -Verb RunAs
+      Start-Process -FilePath 'powershell' -ArgumentList '-Command if (Get-Command docker -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_docker_desktop_installed -Value true -Scope Global }' -Verb RunAs
     EOF
   }
 }
@@ -29,7 +29,7 @@ resource "null_resource" "check_requirements" {
 resource "null_resource" "install_choco" {
   provisioner "local-exec" {
     command = <<EOF
-      powershell -command "Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if (-not (Get-Variable -Name TF_VAR_choco_installed -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\choco-install.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs"
+      Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if (-not (Get-Variable -Name TF_VAR_choco_installed -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\choco-install.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs
     EOF
   }
 }
@@ -37,7 +37,7 @@ resource "null_resource" "install_choco" {
 resource "null_resource" "install_terraform" {
   provisioner "local-exec" {
     command = <<EOF
-      powershell -command "Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if (-not (Get-Variable -Name TF_VAR_terraform_installed -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\terraform-install.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs"
+      Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if (-not (Get-Variable -Name TF_VAR_terraform_installed -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\terraform-install.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs
     EOF
   }
 }
@@ -45,7 +45,7 @@ resource "null_resource" "install_terraform" {
 resource "null_resource" "install_docker_desktop" {
   provisioner "local-exec" {
     command = <<EOF
-      powershell -command "Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if (-not (Get-Variable -Name TF_VAR_docker_desktop_installed -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\docker_desktop-install.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs"
+      Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if (-not (Get-Variable -Name TF_VAR_docker_desktop_installed -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\docker_desktop-install.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs
     EOF
   }
 }
@@ -55,7 +55,7 @@ resource "null_resource" "configure_docker_desktop" {
 
   provisioner "local-exec" {
     command = <<EOF
-      powershell -command "Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if ((Get-Variable -Name TF_VAR_docker_desktop_installed -ErrorAction SilentlyContinue) -or (Get-Command docker -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\docker_desktop-configure.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs"
+      Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if ((Get-Variable -Name TF_VAR_docker_desktop_installed -ErrorAction SilentlyContinue) -or (Get-Command docker -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\docker_desktop-configure.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs
     EOF
   }
 }
