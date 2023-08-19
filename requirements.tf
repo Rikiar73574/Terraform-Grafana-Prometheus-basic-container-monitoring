2def93b90204225ca3ad6181fd2fa95369069dcc
loseryoda <31484379+loseryoda@users.noreply.github.com>
2023-08-19 22:38:20 +0200

    Running scripts workaround

--- requirements.tf
+++ requirements.tf
@@ -19,45 +19,54 @@ variable "docker_desktop_installed" {
 resource "null_resource" "check_requirements" {
   provisioner "local-exec" {
     command = <<EOF
-      Start-Process -FilePath 'powershell' -ArgumentList '-Command if (Get-Command choco -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_choco_installed -Value true -Scope Global }' -Verb RunAs
-      Start-Process -FilePath 'powershell' -ArgumentList '-Command if (Get-Command terraform -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_terraform_installed -Value true -Scope Global }' -Verb RunAs
-      Start-Process -FilePath 'powershell' -ArgumentList '-Command if (Get-Command docker -ErrorAction SilentlyContinue) { Set-Variable -Name TF_VAR_docker_desktop_installed -Value true -Scope Global }' -Verb RunAs
+      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\choco-check.ps1' -Verb RunAs}"
+      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\terraform-check.ps1' -Verb RunAs}"
+      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\docker_desktop-check.ps1' -Verb RunAs}"
     EOF
   }
 }
 
 resource "null_resource" "install_choco" {
+  count = var.choco_installed ? 0 : 1
+
   provisioner "local-exec" {
     command = <<EOF
-      Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if (-not (Get-Variable -Name TF_VAR_choco_installed -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\choco-install.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs
+      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\choco-install.ps1' -Verb RunAs}"
     EOF
   }
 }
 
 resource "null_resource" "install_terraform" {
+  count = var.terraform_installed ? 0 : 1
+
   provisioner "local-exec" {
     command = <<EOF
-      Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if (-not (Get-Variable -Name TF_VAR_terraform_installed -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\terraform-install.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs
+      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\terraform-install.ps1' -Verb RunAs}"
     EOF
   }
 }
 
 resource "null_resource" "install_docker_desktop" {
+  count = var.docker_desktop_installed ? 0 : 1
+
   provisioner "local-exec" {
     command = <<EOF
-      Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if (-not (Get-Variable -Name TF_VAR_docker_desktop_installed -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\docker_desktop-install.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs
+      powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\Terraform_Helpers\docker_desktop-install.ps1' -Verb RunAs}"
     EOF
   }
 }
 
+
+
 resource "null_resource" "configure_docker_desktop" {
   depends_on = [null_resource.install_docker_desktop]
-
-  provisioner "local-exec" {
-    command = <<EOF
-      Start-Process -FilePath 'powershell' -ArgumentList '-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; if ((Get-Variable -Name TF_VAR_docker_desktop_installed -ErrorAction SilentlyContinue) -or (Get-Command docker -ErrorAction SilentlyContinue)) { .\Terraform_Helpers\docker_desktop-configure.ps1 }; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser' -Verb RunAs
-    EOF
-  }
+  count = var.docker_desktop_installed ? 0 : 1
+ provisioner "local-exec" {
+   command = <<EOF
+     powershell -Command "& {Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ${path.module}\\Terraform_Helpers\\docker_desktop-configure.ps1' -Verb RunAs}"
+   EOF
+ }
+ 
 }
 
 
